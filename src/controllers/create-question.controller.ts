7eb737bce22b015685522d42317033ad88f6d217
gustavocotalima <gustavocotalima@hotmail.com>
2023-09-06 17:34:20 -0300

    jwt authentication done

---
+++ src/controllers/create-question.controller.ts
@@ -0,0 +1,28 @@
+import { Body, Controller, Post, UseGuards, UsePipes } from '@nestjs/common'
+import { AuthGuard } from '@nestjs/passport'
+import { JwtAuthGuard } from 'src/auth/jwt-auth.guard'
+import { ZodValidationPipe } from 'src/pipes/zod-validation-pipe'
+import { PrismaService } from 'src/prisma/prisma.service'
+import { z } from 'zod'
+
+const createQuestionBodySchema = z.object({
+  title: z.string(),
+  description: z.string(),
+})
+
+type CreateQuestionBody = z.infer<typeof createQuestionBodySchema>
+
+@Controller('/questions')
+@UseGuards(JwtAuthGuard)
+export class CreateQuestionController {
+  constructor(private prisma: PrismaService) {}
+
+  @Post()
+  @UsePipes(new ZodValidationPipe(createQuestionBodySchema))
+  async handle(
+    @Body()
+    { title, description }: CreateQuestionBody,
+  ) {
+    return { title, description }
+  }
+}
